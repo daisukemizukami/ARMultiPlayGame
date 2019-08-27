d1b6a8fb9ca686d8589686981b70f77aebf855cb
daisukemizukami <d.increment@gmail.com>
2019-08-27 10:33:43 +0900

    positionsyncが毎フレームだと処理に詰まってるので、インプットフィールドからポジション更新intermaltimeを設定可能に

--- Assets/ARGame/Scripts/PositionSync.cs
+++ Assets/ARGame/Scripts/PositionSync.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using WebSocketSharp;
 using UniRx;
@@ -8,38 +9,96 @@ using UnityEngine.UI;
 public class PositionSync : MonoBehaviour
 {
 
-	[SerializeField] private int _WSPort;
-	[SerializeField] private Transform _syncObjTransform;   //Share transform
+    [SerializeField] private int _WSPort;
+    [SerializeField] private Transform _syncObjTransform;   //Share transform
     [SerializeField] GameObject OtherCube;   //Share transform
-
-
     [SerializeField] private SyncPhase _nowPhase;
 
-	private WebSocket ws;
+    private WebSocket ws;
     public InputField IP_inputField;
+    public InputField interval_inputField;
 
-	public enum SyncPhase
-	{
-		Idling,
-		Syncing
-	}
+    [SerializeField]
+    private float tmpTime = 0f;
+    [SerializeField]
+    private float _interval = 0.1f;
 
-	private void Awake()
-	{
-		_nowPhase = SyncPhase.Idling;
+    public enum SyncPhase
+    {
+        Idling,
+        Syncing
+    }
 
-		var cTransformValue = gameObject.ObserveEveryValueChanged(_ => _syncObjTransform.position);
-		cTransformValue.Subscribe(x => OnChangedTargetTransformValue(x));
-	}
+    private void Awake()
+    {
+        _nowPhase = SyncPhase.Idling;
 
-	/// <summary>
-	/// Get Down Start Sync Button
-	/// </summary>
-	public void OnSyncStartButtonDown()
+        //var cTransformValue = gameObject.ObserveEveryValueChanged(_ => _syncObjTransform.position);
+        //cTransformValue.Subscribe(x => OnChangedTargetTransformValue(x));
+
+        //intervalの監視
+        var intervalValue = gameObject.ObserveEveryValueChanged(_ => interval_inputField.text);
+        intervalValue.Subscribe(x => OnChangedTargetIntervalValue(x));
+
+    }
+
+    public void OnChangedTargetTransformValue(Vector3 pos)
+    {
+        if (_nowPhase == SyncPhase.Syncing)
+        {
+            Debug.Log(pos);
+
+            ws.Send(pos.ToString());
+
+        }
+
+    }
+
+    public void OnChangedTargetIntervalValue(String interval)
+    {
+        try
+        {
+            _interval = Convert.ToInt32(interval);
+        }
+        catch (FormatException)
+        {
+            _interval = 0.1f;
+            interval_inputField.text =Convert.ToString (0.1);
+
+        }
+        catch (OverflowException)
+        {
+            _interval = 0.1f;
+            interval_inputField.text = Convert.ToString(0.1);
+        }
+
+
+    }
+    
+
+private void Update()
+    {
+        tmpTime += Time.deltaTime;
+       if( tmpTime > _interval)
+        {
+            OnChangedTargetTransformValue(_syncObjTransform.position);
+
+
+            tmpTime = 0;
+        }
+
+    }
+
+    /// <summary>
+    /// Get Down Start Sync Button
+    /// </summary>
+    public void OnSyncStartButtonDown()
 	{
 		var ca = "ws://" + IP_inputField.text + ":" + _WSPort.ToString();
 		Debug.Log("Connect to " + ca);
 		ws = new WebSocket(ca);
+
+       
 
         var context = System.Threading.SynchronizationContext.Current;
 
@@ -47,14 +106,11 @@ public class PositionSync : MonoBehaviour
         //Add Events
         //On catch message event
         ws.OnMessage += (object sender, MessageEventArgs e) => {
-			print(e.Data);
+			Debug.Log(e.Data);
 
             Vector3 pos = StringToVector3(e.Data);
-            print(e.Data);
-            print(pos.x);
-            print(pos.y);
-            print(pos.z);
-
+            Debug.Log(e.Data);
+     
 
             // Main Threadで実行する.
             context.Post(state =>
@@ -94,18 +150,7 @@ public class PositionSync : MonoBehaviour
 
     }
 
-    public void OnChangedTargetTransformValue(Vector3 pos)
-	{
-		if (_nowPhase == SyncPhase.Syncing)
-		{
-			Debug.Log(pos);
-
-            ws.Send(pos.ToString());
-            //ws.Send(pos);
-
-        }
-       
-	}
+ 
 
     public static Vector3 StringToVector3(string sVector)
     {
