f4993dc0514c3f03d885b322779410262d2c0bd6
daisukemizukami <d.increment@gmail.com>
2019-08-31 13:51:04 +0900

    Jsonクラス追加

---
+++ Assets/ARGame/Scripts/JsonData.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+//namespace PlayerData
+//{
+[Serializable]
+public class JsonData:object
+    {
+	    public string type;
+		public int id;
+
+		public float posX;
+		public float posY;
+		public float posZ;
+
+		public float eulerAngleX;
+		public float eulerAngleY;
+		public float eulerAngleZ;
+
+	}
+
+//}
